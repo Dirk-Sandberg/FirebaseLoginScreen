60390b322cf016ca6a8a44bca5fd3d04dc4d8b9b
eriksandberg <eriksandbergum@gmail.com>
2019-04-30 15:41:25 -0700

    Sign in and password reset both working. Updated README.

--- FirebaseLoginScreen/firebaseloginscreen.py
+++ FirebaseLoginScreen/firebaseloginscreen.py
@@ -24,14 +24,18 @@ class FirebaseLoginScreen(Screen, EventDispatcher):
 
     # Firebase Authentication Credentials - what developers want to retrieve
     email = ""
-    user_token = ""
+    refresh_token = ""
+    localId = ""
+    idToken = ""
 
     # Private properties used to update parts of the login screen
-    sign_up_error_msg = StringProperty()
+    sign_up_msg = StringProperty()
+    sign_in_msg = StringProperty()
     email_exists = BooleanProperty(False)
+    email_not_found = BooleanProperty(False)
 
     def on_login_success(self, *args):
-        """Overwrite this method to switch to your screen.
+        """Overwrite this method to switch to your app's home screen.
         """
         print("Logged in successfully", args)
 
@@ -43,49 +47,77 @@ class FirebaseLoginScreen(Screen, EventDispatcher):
         signup_url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=" + self.web_api_key
         signup_payload = dumps({"email": email, "password": password, "returnSecureToken": "true"})
 
-        sign_up_request = UrlRequest(signup_url, req_body=signup_payload,
-                                     on_success=self.sign_up_success,
-                                     on_failure=self.sign_up_failure,
-                                     on_error=self.sign_up_error)
+        UrlRequest(signup_url, req_body=signup_payload,
+                                 on_success=self.successful_login,
+                                 on_failure=self.sign_up_failure,
+                                 on_error=self.sign_up_error)
 
-    def sign_up_success(self, *args):
+    def successful_login(self, urlrequest, log_in_data):
         """Collects info from Firebase upon successfully registering a new user.
         """
-        sign_up_data = args[1]
-        print(sign_up_data)
-        self.refresh_token = sign_up_data['refreshToken']
-        self.localId = ''
-        self.idToken = ''
-        self.email = sign_up_data['email']
+        self.refresh_token = log_in_data['refreshToken']
+        self.localId = log_in_data['localId']
+        self.idToken = log_in_data['idToken']
+        self.email = log_in_data['email']
+        self.login_success = True
+        print(log_in_data)
 
-    def sign_up_error(self, *args):
-        print("Error")
-        print(args)
-
-    def sign_up_failure(self, *args):
-        failure_data = args[1]
+    def sign_up_failure(self, urlrequest, failure_data):
         msg = failure_data['error']['message'].replace("_", " ").capitalize()
         # Check if the error msg is the same as the last one
-        if msg == self.sign_up_error_msg:
+        if msg == self.sign_up_msg:
             msg = " " + msg + " "
             # Need to modify it somehow to make the error popup display
-        self.sign_up_error_msg = msg
+        self.sign_up_msg = msg
         if msg == "Email exists":
             self.email_exists = True
 
-
+    def sign_up_error(self, *args):
+        print("Sign up Error")
+        print(args)
 
     def sign_in(self, email, password):
         """If you don't want to use Firebase, just overwrite this method and
         do whatever you need to do to sign the user in with their email and
         password.
         """
-        print("Sign In")
-        self.login_success = True # automatically triggers on_login_success, which users can overwrite
+        sign_in_url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=" + self.web_api_key
+        sign_in_payload = dumps({"email": email, "password": password, "returnSecureToken": True})
+
+        UrlRequest(sign_in_url, req_body=sign_in_payload,
+                                on_success=self.successful_login,
+                                on_failure=self.sign_in_failure,
+                                on_error=self.sign_in_error)
+
+    def sign_in_failure(self, urlrequest, failure_data):
+        msg = failure_data['error']['message'].replace("_", " ").capitalize()
+        # Check if the error msg is the same as the last one
+        if msg == self.sign_in_msg:
+            msg = " " + msg + " "
+            # Need to modify it somehow to make the error popup display
+        self.sign_in_msg = msg
+        print(msg)
+        if msg == "Email not found":
+            self.email_not_found = True
+
+
+    def sign_in_error(self, *args):
+        print("Sign in error")
+        print(args)
 
     def reset_password(self, email):
         """If you don't want to use Firebase, just overwrite this method and
         do whatever you need to do to reset a user's password from their email.
         """
-        print("Reset pw")
+        reset_pw_url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/getOobConfirmationCode?key=" + self.web_api_key
+        reset_pw_data = dumps({"email": email, "requestType": "PASSWORD_RESET"})
 
+        UrlRequest(reset_pw_url, req_body=reset_pw_data,
+                                on_success=self.successful_reset,
+                                on_failure=self.sign_in_failure,
+                                on_error=self.sign_in_error)
+
+    def successful_reset(self, urlrequest, reset_data):
+        print("Reset password!")
+        self.sign_in_msg = "Reset password instructions sent to your email."
+        print(reset_data)
--- README.md
+++ README.md
@@ -1,34 +1,65 @@
 # FirebaseLoginScreen
-### UNDER DEVELOPMENT - UNSTABLE ###
+#### UNDER DEVELOPMENT - UNSTABLE ###
 Complete login screen with backend included for Kivy apps
-
 
 This package lets you very easily incorporate a login screen into your app.
 
---------- USAGE ---------
+<h2><b>--------- USAGE ---------</b></h1>
 
-In your main.py file, include the following import statement:
-import FirebaseLoginScreen
+In your main.py file, include the following import statement:<br>
+`import FirebaseLoginScreen`
 
 Then, in the kv file where you want to use the login screen, include this statement:
-#:include FirebaseLoginScreen/firebaseloginscreen.kv
+<br>`#:include FirebaseLoginScreen/firebaseloginscreen.kv`
 
 Now you can instantiate the FirebaseLoginScreen class, which inherits from the
 Kivy Screen class. That means you need to add it to your ScreenManager.
 
-Example:
-    `main.kv`
+Typically, when making requests to your database to get data for your user, you
+identify the user by their `localId`. You can use the `idToken` to authenticate
+a user's request to the database if you have set up Firebase <b>Rules</b>. Both
+of these variables are automatically retrieved for you by `FirebaseLoginScreen`.<br>
+Code Example:<br>
+`main.py`
+
+    from kivy.app import App
+    
+    from FirebaseLoginScreen.firebaseloginscreen import FirebaseLoginScreen
+    
+    class MainApp(App):
+        pass
+        
+    MainApp().run()
+
+
+
+`main.kv`
+    
+    #:include FirebaseLoginScreen/firebaseloginscreen.kv
+
     ScreenManager:
         FirebaseLoginScreen:
             id: firebase_login_screen
-            web_addr: "gweomg-wewegw"
-            secret_api_key: "wkgmwanrg"
-            on_login_success:
+            secret_api_key: "your_web_api_key_from_firebase" # Found in Firebase -> Project Settings -> Web API Key
+            primary_color: (1,0,0)   # Customize the
+            secondary_color: (0,1,0) # _ color theme
+            tertiary_color: (0,0,1)  # __ of the login screens
+            on_login_success: # Meant to be overwritten
+                # Get the important user info
+                app.user_email = self.email
+                app.user_localId = self.localId
+                app.user_
                 app.do_whatever()  # Your function when they log in
 
 
---------- NOTE ---------
-Your Firebase project must be allowed to register user's through an email and
-password. You can allow this by going to the Authentication portion of your
+<h5><b>--------- NOTE: ENABLE EMAIL AUTHENTICATION ---------</b></h5>
+Your Firebase project must be allowed to register users through an email and
+password. You can set this up easily by going to the Authentication portion of your
 Firebase project.
+
+<h5><b>--------- NOTE: CUSTOMIZE RESET PASSWORD EMAIL ---------</b></h5>
+You can customize the email that is sent to users when they want to reset their email.
+Go to your Firebase project, then go to <b>Authentication</b>, then click on <b>Templates</b>,
+then click on <b>Password reset</b>
+
 
