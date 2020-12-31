5dc3f66bdb0f00b49785848dc687d57bc284ee2c
erik sandberg <eriksandbergum@gmail.com>
2020-12-31 13:54:31 -0600

    Back button is now chevron in the top left

--- firebaseloginscreen/createaccountscreen.py
+++ firebaseloginscreen/createaccountscreen.py
@@ -1,5 +1,8 @@
-from kivy.uix.screenmanager import Screen
+from kivy.uix.screenmanager import Screen, SlideTransition
 
 class CreateAccountScreen(Screen):
-    pass
+    def go_back(self):
+        self.parent.transition = SlideTransition(direction="right")
+        self.parent.current = self.parent.current = "welcome_screen"
+        self.parent.transition = SlideTransition(direction="left")
 
